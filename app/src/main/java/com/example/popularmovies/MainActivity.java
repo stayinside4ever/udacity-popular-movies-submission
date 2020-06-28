cc817cbcfd55a85193b6750083dcd05662fa7b93
stayinside4ever <deina.kotane@gmail.com>
2020-06-28 18:19:11 +0100

    add a loading indicator to the posters grid before the network request is resolved

--- app/src/main/java/com/example/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/popularmovies/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.popularmovies;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -12,16 +14,21 @@ import com.example.popularmovies.network.NetworkUtils;
 import java.util.ArrayList;
 import java.util.List;
 
+enum LoadingState { LOADING, FINISHED }
+
 public class MainActivity extends AppCompatActivity{
     NetworkUtils networkUtils;
     PosterListAdapter adapter;
+    RecyclerView postersRecyclerView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        RecyclerView postersRecyclerView = (RecyclerView) findViewById(R.id.rv_posters_list);
+        postersRecyclerView = (RecyclerView) findViewById(R.id.rv_posters_list);
+        progressBar = (ProgressBar) findViewById(R.id.pb_posters_list);
 
         GridLayoutManager layoutManager = new GridLayoutManager(this, 3);
         postersRecyclerView.setLayoutManager(layoutManager);
@@ -33,10 +40,25 @@ public class MainActivity extends AppCompatActivity{
             @Override
             public void onMoviesFetched(List<MovieEntity> movies) {
                 adapter.update(movies);
+                setLoadingState(LoadingState.FINISHED);
             }
         });
 
         networkUtils.getMoviesFromNetwork("TODO");
+        setLoadingState(LoadingState.LOADING);
+    }
+
+    private void setLoadingState(LoadingState state) {
+        switch (state) {
+            case LOADING:
+                postersRecyclerView.setVisibility(View.GONE);
+                progressBar.setVisibility(View.VISIBLE);
+                break;
+            case FINISHED:
+                progressBar.setVisibility(View.GONE);
+                postersRecyclerView.setVisibility(View.VISIBLE);
+                break;
+        }
     }
 
 }
