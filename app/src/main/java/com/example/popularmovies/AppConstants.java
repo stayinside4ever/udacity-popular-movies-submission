310a313cdd38b2ee16336de50db6d0afa9262f08
stayinside4ever <deina.kotane@gmail.com>
2020-06-30 13:30:14 +0100

    add appconstants file

---
+++ app/src/main/java/com/example/popularmovies/AppConstants.java
@@ -0,0 +1,7 @@
+package com.example.popularmovies;
+
+public final class AppConstants {
+    public static final String API_KEY = ""; // TODO: add api key here
+    public static final String IMAGES_BASE_URL = "http://image.tmdb.org/t/p/";
+    public static final String QUERIES_BASE_URL = "http://api.themoviedb.org/3/";
+}
